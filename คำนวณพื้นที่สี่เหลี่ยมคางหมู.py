f01e5bc8873a88cef1f0e5917fdc1e20d86aab18
Phornpawee26927 <164387@wsk.ac.th>
2026-06-16 16:09:04 +0000

    คำนวณพื้นที่สี่เหลี่ยมคางหมู.py

---
+++ คำนวณพื้นที่สี่เหลี่ยมคางหมู.py
@@ -0,0 +1,6 @@
+#คำนวณพื้นที่สี่เหลี่ยมคางหมู
+top_base = float(input("กรุณาป้อนความยาวฐาน"))
+bottom_base = float(input("กรุณาป้อนความยาวฐานล่าง"))
+height = float(input("กรุณาป้อนความสูง"))
+area = 1/2*(top_base+bottom_base)*height
+print("พื้นที่สี่เหลี่ยมคางหมู=",area)
